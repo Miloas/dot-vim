dda99843d1aa0cf659be1793d96947d455509738
Miloas <0x9357@gmail.com>
2023-04-17 02:51:23 +0800

    feat: add prettier format

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,3 +1,19 @@
+-- Prettier function for formatter
+local prettier = function()
+  return {
+    exe = "prettier",
+    args = {
+      "--config-precedence",
+      "prefer-file",
+      -- you can add more global setup here
+      "--stdin-filepath",
+      vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+    },
+    stdin = true,
+    try_node_modules = true,
+  }
+end
+
 return {
   {
     "L3MON4D3/LuaSnip",
@@ -51,6 +67,20 @@ return {
         log_level = vim.log.levels.WARN,
         -- All formatter configurations are opt-in
         filetype = {
+          typescriptreact = { prettier },
+          javascriptreact = { prettier },
+          javascript = { prettier },
+          typescript = { prettier },
+          json = { prettier },
+          jsonc = { prettier },
+          html = { prettier },
+          css = { prettier },
+          scss = { prettier },
+          graphql = { prettier },
+          markdown = { prettier },
+          vue = { prettier },
+          astro = { prettier },
+          yaml = { prettier },
           -- Formatter configurations for filetype "lua" go here
           -- and will be executed in order
           lua = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,12 @@ return {
         sourcekit = {},
         zls = {},
         tsserver = {},
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
+        },
         pyright = {},
         lua_ls = {
           settings = {
@@ -46,6 +52,15 @@ return {
         tsserver = function(_, opts)
           require("typescript").setup({ server = opts })
           return true
+        end,
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
+                vim.cmd("EslintFixAll")
+              end
+            end,
+          })
         end,
       },
     },
