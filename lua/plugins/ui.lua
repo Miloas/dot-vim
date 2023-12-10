efdfba0ce02ce0137c3d0d614f050b6f75552232
Miloas <0x9357@gmail.com>
2023-12-10 19:55:00 +0800

    notification ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -44,6 +44,17 @@ return {
     }
   },
 
+  -- notification
+  {
+    'j-hui/fidget.nvim',
+    event = 'VeryLazy',
+    opts = {
+      notification = {
+        override_vim_notify = true
+      }
+    }
+  },
+
   -- icons
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
